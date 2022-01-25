43472e5a531661bcdfb1fcf2da48b8e8dcb2a1be
kucicm <10778553+KucicM@users.noreply.gith       ub.com>
2022-01-25 21:30:02 +0100

    refactor agents

--- pkg/crawler/agent.go
+++ pkg/crawler/agent.go
@@ -2,42 +2,21 @@
 package crawler
 
 import (
-	"io/ioutil"
 	"log"
-	"sync"
-
-	"gopkg.in/yaml.v2"
 )
 
 type HttpAgents struct {
-	id     int
 	Agents []string `yaml:"User-Agents"`
-	mu     sync.Mutex
 }
 
-func newHttpAgents(filePath string) *HttpAgents {
-	cfg, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		log.Fatal(err)
+func (a *HttpAgents) getIter() func() string {
+	if len(a.Agents) == 0 {
+		log.Fatal("No agents")
 	}
 
-	agents := &HttpAgents{}
-	if err := yaml.Unmarshal(cfg, &agents); err != nil {
-		log.Fatal(err)
+	id := 0
+	return func() string {
+		id = (id + 1) % len(a.Agents)
+		return a.Agents[id]
 	}
-
-	if len(agents.Agents) == 0 {
-		log.Fatal("Zero agents not allowed")
-	}
-
-	return agents
-}
-
-func (a *HttpAgents) next() string {
-	a.mu.Lock()
-	defer a.mu.Unlock() // overkill
-
-	ret := a.Agents[a.id]
-	a.id = (a.id + 1) % len(a.Agents)
-	return ret
 }
--- pkg/crawler/agent_test.go
+++ pkg/crawler/agent_test.go
@@ -1,78 +1,70 @@
 package crawler
 
 import (
-	"io/ioutil"
 	"sync"
-	"syscall"
 	"testing"
+
+	"gopkg.in/yaml.v2"
 )
 
 func TestRoundRobinAgents(t *testing.T) {
-	fileName := "tmp.yml"
-	f, err := ioutil.TempFile("", fileName)
-	if err != nil {
-		panic(err)
-	}
-	defer syscall.Unlink(f.Name())
-
 	cfg := `
     User-Agents:
         - "Agent-1"
         - "Agent-2"
         - "Agent-3"
     `
-	ioutil.WriteFile(f.Name(), []byte(cfg), 0644)
 
-	agents := newHttpAgents(f.Name())
-
-	agent := agents.next()
-	if agent != "Agent-1" {
-		t.Error("Expected: Agent-1 got: ", agent)
+	agents := HttpAgents{}
+	if err := yaml.Unmarshal([]byte(cfg), &agents); err != nil {
+		t.Error(err)
 	}
 
-	agent = agents.next()
+	iter := agents.getIter()
+
+	agent := iter()
 	if agent != "Agent-2" {
 		t.Error("Expected: Agent-2 got: ", agent)
 	}
 
-	agent = agents.next()
+	agent = iter()
 	if agent != "Agent-3" {
 		t.Error("Expected: Agent-3 got: ", agent)
 	}
 
-	agent = agents.next()
+	agent = iter()
 	if agent != "Agent-1" {
 		t.Error("Expected: Agent-1 got: ", agent)
+	}
+
+	agent = iter()
+	if agent != "Agent-2" {
+		t.Error("Expected: Agent-2 got: ", agent)
 	}
 }
 
 func TestAgentsRace(t *testing.T) {
-	fileName := "tmp.yml"
-	f, err := ioutil.TempFile("", fileName)
-	if err != nil {
-		panic(err)
-	}
-	defer syscall.Unlink(f.Name())
-
 	cfg := `
     User-Agents:
         - "Agent-1"
         - "Agent-2"
         - "Agent-3"
     `
-	ioutil.WriteFile(f.Name(), []byte(cfg), 0644)
 
-	agents := newHttpAgents(f.Name())
+	agents := HttpAgents{}
+	if err := yaml.Unmarshal([]byte(cfg), &agents); err != nil {
+		t.Error(err)
+	}
 
 	wg := sync.WaitGroup{}
 	wg.Add(10)
 	defer wg.Wait()
 
 	for i := 0; i < 10; i++ {
-		go func(w *sync.WaitGroup, agents *HttpAgents) {
+		go func(w *sync.WaitGroup, iter func() string) {
 			defer w.Done()
-			agents.next()
-		}(&wg, agents)
+			iter()
+		}(&wg, agents.getIter())
 	}
 
 }
